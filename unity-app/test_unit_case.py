e3d9b912c02757c624143ca8dab285f1ba8937a4
Miroslav Dzurik <dzurik.miroslav@gmail.com>
2024-09-23 16:07:51 +0200

    pytest

--- unity-app/test_unit_case.py
+++ unity-app/test_unit_case.py
@@ -1,15 +1,11 @@
-from pytest_embedded_idf.dut import IdfDut
+import pytest
+from pytest_embedded import Dut
 
-def test_app(dut: IdfDut):
-    dut.expect_unity_test_output()
 
-# import pytest
-# from pytest_embedded import Dut
-
-# def test_unit_test(dut: Dut, test_name: str) -> None:
-#     if test_name == "TEST_ALL":
-#         dut.run_all_single_board_cases()
-#     elif test_name[:11] == "TEST_GROUP=":
-#         dut.run_all_single_board_cases(test_name[12:])
-#     else:
-#         dut.run_single_board_case(test_name)
+def test_unit_test(dut: Dut, test_name: str) -> None:
+    if test_name == "TEST_ALL":
+        dut.run_all_single_board_cases()
+    elif test_name[:11] == "TEST_GROUP=":
+        dut.run_all_single_board_cases(test_name[12:])
+    else:
+        dut.run_single_board_case(test_name)
