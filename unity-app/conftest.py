13fe7eebc2480e4a2715933da32497f5542ff554
Miroslav Dzurik <dzurik.miroslav@gmail.com>
2024-09-23 15:44:46 +0200

    pytest

--- unity-app/conftest.py
+++ unity-app/conftest.py
@@ -1,10 +1,10 @@
-def pytest_addoption(parser):
-    parser.addoption("--test-name", action="store", default="*")
+# def pytest_addoption(parser):
+#     parser.addoption("--test-name", action="store", default="*")
 
 
-def pytest_generate_tests(metafunc):
-    # This is called for every test. Only get/set command line arguments
-    # if the argument is specified in the list of test "fixturenames".
-    option_value = metafunc.config.option.test_name
-    if 'test_name' in metafunc.fixturenames and option_value is not None:
-        metafunc.parametrize("test_name", [option_value])
+# def pytest_generate_tests(metafunc):
+#     # This is called for every test. Only get/set command line arguments
+#     # if the argument is specified in the list of test "fixturenames".
+#     option_value = metafunc.config.option.test_name
+#     if 'test_name' in metafunc.fixturenames and option_value is not None:
+#         metafunc.parametrize("test_name", [option_value])
