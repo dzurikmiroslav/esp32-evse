c0808fcd537fd9219eb728bbd864fd4d7ca05264
Miroslav Dzurik <dzurik.miroslav@gmail.com>
2026-02-17 15:46:07 +0100

    WiFi 802.11kv neighborhood report

--- components/network/src/wifi.c
+++ components/network/src/wifi.c
@@ -3,8 +3,11 @@
 #include <esp_event.h>
 #include <esp_log.h>
 #include <esp_mac.h>
+#include <esp_mbo.h>
 #include <esp_netif.h>
+#include <esp_rrm.h>
 #include <esp_wifi.h>
+#include <esp_wnm.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/event_groups.h>
 #include <freertos/task.h>
@@ -23,6 +26,24 @@
 #define NVS_STATIC_GATEWAY "stat_gateway"
 #define NVS_STATIC_NETMASK "stat_netmask"
 
+#ifndef WLAN_EID_MEASURE_REPORT
+#define WLAN_EID_MEASURE_REPORT 39
+#endif
+#ifndef MEASURE_TYPE_LCI
+#define MEASURE_TYPE_LCI 9
+#endif
+#ifndef MEASURE_TYPE_LOCATION_CIVIC
+#define MEASURE_TYPE_LOCATION_CIVIC 11
+#endif
+#ifndef WLAN_EID_NEIGHBOR_REPORT
+#define WLAN_EID_NEIGHBOR_REPORT 52
+#endif
+#ifndef ETH_ALEN
+#define ETH_ALEN 6
+#endif
+#define MAX_LCI_CIVIC_LEN 256 * 2 + 1
+#define MAX_NEIGHBOR_LEN  512
+
 _Static_assert(sizeof(((wifi_config_t*)0)->sta.ssid) == WIFI_SSID_SIZE, "Wrong SSID size");
 _Static_assert(sizeof(((wifi_config_t*)0)->sta.password) == WIFI_PASSWORD_SIZE, "Wrong password size");
 
@@ -34,7 +55,14 @@ static esp_netif_t* sta_netif;
 
 static esp_netif_t* ap_netif;
 
+static bool neighbor_report_active = false;
+
 EventGroupHandle_t wifi_event_group;
+
+static inline uint32_t WPA_GET_LE32(const uint8_t* a)
+{
+    return ((uint32_t)a[3] << 24) | (a[2] << 16) | (a[1] << 8) | a[0];
+}
 
 static void sta_try_connect(void)
 {
@@ -57,6 +85,20 @@ static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_
             xEventGroupClearBits(wifi_event_group, WIFI_AP_DISCONNECTED_BIT);
             xEventGroupSetBits(wifi_event_group, WIFI_AP_CONNECTED_BIT);
         }
+        if (event_id == WIFI_EVENT_STA_CONNECTED) {
+            if (esp_rrm_is_rrm_supported_connection()) {
+                ESP_LOGI(TAG, "RRM supported");
+                esp_rrm_send_neighbor_report_request();
+                neighbor_report_active = true;
+            } else {
+                ESP_LOGI(TAG, "RRM not supported");
+            }
+            if (esp_wnm_is_btm_supported_connection()) {
+                ESP_LOGI(TAG, "BTM supported");
+            } else {
+                ESP_LOGI(TAG, "BTM not supported");
+            }
+        }
         if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
             ESP_LOGI(TAG, "AP STA disconnected");
             wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*)event_data;
@@ -65,13 +107,21 @@ static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_
             xEventGroupSetBits(wifi_event_group, WIFI_AP_DISCONNECTED_BIT);
         }
         if (event_id == WIFI_EVENT_STA_DISCONNECTED) {
-            ESP_LOGI(TAG, "STA disconnected");
-            xEventGroupClearBits(wifi_event_group, WIFI_STA_CONNECTED_BIT);
-            xEventGroupSetBits(wifi_event_group, WIFI_STA_DISCONNECTED_BIT);
-            sta_try_connect();
+            wifi_event_sta_disconnected_t* event = event_data;
+            if (event->reason == WIFI_REASON_ROAMING) {
+                ESP_LOGI(TAG, "Station roaming, do nothing");
+            } else {
+                ESP_LOGI(TAG, "STA disconnected");
+                neighbor_report_active = false;
+                xEventGroupClearBits(wifi_event_group, WIFI_STA_CONNECTED_BIT);
+                xEventGroupSetBits(wifi_event_group, WIFI_STA_DISCONNECTED_BIT);
+                neighbor_report_active = false;
+                sta_try_connect();
+            }
         }
         if (event_id == WIFI_EVENT_STA_START) {
             ESP_LOGI(TAG, "STA start");
+            neighbor_report_active = false;
             sta_try_connect();
         }
     } else if (event_base == IP_EVENT) {
@@ -87,6 +137,203 @@ static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_
             xEventGroupSetBits(wifi_event_group, WIFI_STA_CONNECTED_BIT);
         }
     }
+}
+
+static char* get_btm_neighbor_list(uint8_t* report, size_t report_len)
+{
+    size_t len = 0;
+    const uint8_t* data;
+    int ret = 0;
+
+    char* lci = NULL;
+    char* civic = NULL;
+    /*
+     * Neighbor Report element (IEEE P802.11-REVmc/D5.0)
+     * BSSID[6]
+     * BSSID Information[4]
+     * Operating Class[1]
+     * Channel Number[1]
+     * PHY Type[1]
+     * Optional Subelements[variable]
+     */
+#define NR_IE_MIN_LEN (ETH_ALEN + 4 + 1 + 1 + 1)
+
+    if (!report || report_len == 0) {
+        ESP_LOGI(TAG, "RRM neighbor report is not valid");
+        return NULL;
+    }
+
+    char* buf = calloc(1, MAX_NEIGHBOR_LEN);
+    if (!buf) {
+        ESP_LOGE(TAG, "Memory allocation for neighbor list failed");
+        goto cleanup;
+    }
+    data = report;
+
+    while (report_len >= 2 + NR_IE_MIN_LEN) {
+        const uint8_t* nr;
+        lci = (char*)malloc(sizeof(char) * MAX_LCI_CIVIC_LEN);
+        if (!lci) {
+            ESP_LOGE(TAG, "Memory allocation for lci failed");
+            goto cleanup;
+        }
+        civic = (char*)malloc(sizeof(char) * MAX_LCI_CIVIC_LEN);
+        if (!civic) {
+            ESP_LOGE(TAG, "Memory allocation for civic failed");
+            goto cleanup;
+        }
+        uint8_t nr_len = data[1];
+        const uint8_t *pos = data, *end;
+
+        if (pos[0] != WLAN_EID_NEIGHBOR_REPORT || nr_len < NR_IE_MIN_LEN) {
+            ESP_LOGI(TAG, "CTRL: Invalid Neighbor Report element: id=%u len=%u", data[0], nr_len);
+            ret = -1;
+            goto cleanup;
+        }
+
+        if (2U + nr_len > report_len) {
+            ESP_LOGI(TAG, "CTRL: Invalid Neighbor Report element: id=%u len=%zu nr_len=%u", data[0], report_len, nr_len);
+            ret = -1;
+            goto cleanup;
+        }
+        pos += 2;
+        end = pos + nr_len;
+
+        nr = pos;
+        pos += NR_IE_MIN_LEN;
+
+        lci[0] = '\0';
+        civic[0] = '\0';
+        while (end - pos > 2) {
+            uint8_t s_id, s_len;
+
+            s_id = *pos++;
+            s_len = *pos++;
+            if (s_len > end - pos) {
+                ret = -1;
+                goto cleanup;
+            }
+            if (s_id == WLAN_EID_MEASURE_REPORT && s_len > 3) {
+                /* Measurement Token[1] */
+                /* Measurement Report Mode[1] */
+                /* Measurement Type[1] */
+                /* Measurement Report[variable] */
+                switch (pos[2]) {
+                case MEASURE_TYPE_LCI:
+                    if (lci[0]) break;
+                    memcpy(lci, pos, s_len);
+                    break;
+                case MEASURE_TYPE_LOCATION_CIVIC:
+                    if (civic[0]) break;
+                    memcpy(civic, pos, s_len);
+                    break;
+                }
+            }
+
+            pos += s_len;
+        }
+
+        ESP_LOGI(TAG,
+                 "RMM neighbor report bssid=" MACSTR " info=0x%" PRIx32 " op_class=%u chan=%u phy_type=%u%s%s%s%s",
+                 MAC2STR(nr),
+                 WPA_GET_LE32(nr + ETH_ALEN),
+                 nr[ETH_ALEN + 4],
+                 nr[ETH_ALEN + 5],
+                 nr[ETH_ALEN + 6],
+                 lci[0] ? " lci=" : "",
+                 lci,
+                 civic[0] ? " civic=" : "",
+                 civic);
+
+        /* neighbor start */
+        len += snprintf(buf + len, MAX_NEIGHBOR_LEN - len, " neighbor=");
+        /* bssid */
+        len += snprintf(buf + len, MAX_NEIGHBOR_LEN - len, MACSTR, MAC2STR(nr));
+        /* , */
+        len += snprintf(buf + len, MAX_NEIGHBOR_LEN - len, ",");
+        /* bssid info */
+        len += snprintf(buf + len, MAX_NEIGHBOR_LEN - len, "0x%04" PRIx32 "", WPA_GET_LE32(nr + ETH_ALEN));
+        len += snprintf(buf + len, MAX_NEIGHBOR_LEN - len, ",");
+        /* operating class */
+        len += snprintf(buf + len, MAX_NEIGHBOR_LEN - len, "%u", nr[ETH_ALEN + 4]);
+        len += snprintf(buf + len, MAX_NEIGHBOR_LEN - len, ",");
+        /* channel number */
+        len += snprintf(buf + len, MAX_NEIGHBOR_LEN - len, "%u", nr[ETH_ALEN + 5]);
+        len += snprintf(buf + len, MAX_NEIGHBOR_LEN - len, ",");
+        /* phy type */
+        len += snprintf(buf + len, MAX_NEIGHBOR_LEN - len, "%u", nr[ETH_ALEN + 6]);
+        /* optional elements, skip */
+
+        data = end;
+        report_len -= 2 + nr_len;
+
+        if (lci) {
+            free(lci);
+            lci = NULL;
+        }
+        if (civic) {
+            free(civic);
+            civic = NULL;
+        }
+    }
+
+cleanup:
+    if (lci) {
+        free(lci);
+    }
+    if (civic) {
+        free(civic);
+    }
+
+    if (ret < 0) {
+        free(buf);
+        buf = NULL;
+    }
+    return buf;
+}
+
+static void neighbor_report_recv_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+    if (!neighbor_report_active) {
+        ESP_LOGV(TAG, "Neighbor report received but not triggered by us");
+        return;
+    }
+    if (!event_data) {
+        ESP_LOGE(TAG, "No event data received for neighbor report");
+        return;
+    }
+    neighbor_report_active = false;
+    uint8_t cand_list = 0;
+    wifi_event_neighbor_report_t* neighbor_report_event = (wifi_event_neighbor_report_t*)event_data;
+    uint8_t* pos = (uint8_t*)neighbor_report_event->report;
+    char* neighbor_list = NULL;
+    if (!pos) {
+        ESP_LOGE(TAG, "Neighbor report is empty");
+        return;
+    }
+    uint8_t report_len = neighbor_report_event->report_len;
+    /* dump report info */
+    ESP_LOGD(TAG, "rrm: neighbor report len=%d", report_len);
+    ESP_LOG_BUFFER_HEXDUMP(TAG, pos, report_len, ESP_LOG_DEBUG);
+
+    /* create neighbor list */
+    neighbor_list = get_btm_neighbor_list(pos + 1, report_len - 1);
+    /* In case neighbor list is not present issue a scan and get the list from that */
+    if (!neighbor_list) {
+        /* issue scan */
+        wifi_scan_config_t params;
+        memset(&params, 0, sizeof(wifi_scan_config_t));
+        if (esp_wifi_scan_start(&params, true) < 0) {
+            goto cleanup;
+        }
+        /* cleanup from net802.11 */
+        esp_wifi_clear_ap_list();
+        cand_list = 1;
+    }
+    /* send AP btm query, this will cause STA to roam as well */
+    esp_wnm_send_bss_transition_mgmt_query(REASON_FRAME_LOSS, neighbor_list, cand_list);
+cleanup:
+    if (neighbor_list) free(neighbor_list);
 }
 
 static esp_err_t wifi_restart(void)
@@ -124,8 +371,8 @@ static esp_err_t wifi_restart(void)
                 {
                     .btm_enabled = 1,
                     .rm_enabled = 1,
-                    .mbo_enabled = 1,
-                    .ft_enabled = 1,
+                    // .mbo_enabled = 1,
+                    // .ft_enabled = 1,
                     .pmf_cfg =
                         {
                             .capable = true,
@@ -192,6 +439,7 @@ void wifi_init(void)
 
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_NEIGHBOR_REP, &neighbor_report_recv_handler, NULL));
 
     if (wifi_is_static_enabled()) {
         ESP_ERROR_CHECK(esp_netif_dhcpc_stop(sta_netif));
